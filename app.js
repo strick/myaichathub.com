71ad5188535db52e9e13ffb3027fcd07bd8c2966
Brian Strickland <bstrick@gmail.com>
2023-08-18 11:28:51 -0400

    Updated pages to use global sitename

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ mongoose.connect(DB_CONNECTION, {
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.locals.siteName = "My AI ChatHub";
+
 // Set up EJS as the templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // assuming your templates are in a 'views' directory
--- routes/main.js
+++ routes/main.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     req.session.user = '';
-    res.render('index', { title: 'My AI Chat Hub' });
+    res.render('index', { title: req.app.locals.siteName });
 });
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
 
-    res.render('dashboard', { title: 'Dashboard - My AI Chat Hub', user: req.session.passport.user });
+    res.render('dashboard', { title: 'Dashboard - My AI ChatHub', user: req.session.passport.user });
 });
 
 module.exports = router;
